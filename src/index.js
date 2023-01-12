357f8994f698f7b8f921a55c3635afdc5371a15f
Sani Rogers <sanirogers138@gmail.com>
2023-01-12 16:02:28 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector("#create-task-form")
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const textBox = document.querySelector("#new-task-description");
+    const textBoxValue = textBox.value;
+    const list = document.querySelector("#tasks");
+    const newListItem = document.createElement("li");
+    newListItem.textContent = textBoxValue;
+    list.append(newListItem);
+    textBox.value = null;
+    console.log(textBoxValue);
+  })
 });
